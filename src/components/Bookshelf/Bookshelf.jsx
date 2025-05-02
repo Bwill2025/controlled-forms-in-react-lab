ac96cc0a640c148b7bbb6f3c54caffb76af059f4
Bwill2025 <bw180mylyfe@outlook.com>
2025-05-01 20:59:34 -0400

    “complete”

---
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+
+const Bookshelf = () => {
+    const [books, setBooks] = useState([]);
+    
+    const [newBook, setNewBook] = useState({ title: "", author: ""});
+    
+    const handleInputChange = (event) => {
+       
+      setNewBook({...newBook, [event.target.name]
+        : event.target.value
+      }); 
+    }
+    const handleSubmit = (event, newBook) => {
+        event.preventDefault();
+        
+            setBooks([...books, newBook]);
+            setNewBook({ title: "", author: "" });
+        
+    };
+    
+    
+      return (
+        <div className="bookshelfDiv">
+            <div className="formDiv">
+                <h3>Add a Book</h3>
+                <form onSubmit={(event) => handleSubmit(event, newBook)}>
+        <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={newBook.title}
+            onChange={handleInputChange}
+            required
+          />  
+          <input
+            type="text"
+            name="author"
+            placeholder="Author"
+            value={newBook.author}
+            onChange={handleInputChange}
+            required
+          />
+          <button type="submit">Add Book</button>
+        </form>
+      </div>
+
+      <div className="bookCardsDiv">
+        {books.map((book, index) => (
+          <div key={index} className="bookCard">
+            <h4>{book.title}</h4>
+            <p>{book.author}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+    
+    export default Bookshelf;
+    
+    
+    
